3bb2c5604c53d98e195fc8ff74b23fc395b298fd
ReConsolidated <gracjanpasik@gmail.com>
2023-05-15 21:59:24 +0200

    asddasdasads

--- src/main/java/io/github/reconsolidated/weaskedapi/authentication/AppUser/AppUserRole.java
+++
@@ -1,5 +0,0 @@
-package io.github.reconsolidated.weaskedapi.authentication.AppUser;
-
-public enum AppUserRole {
-    USER, ADMIN
-}
--- src/main/java/io/github/reconsolidated/weaskedapi/authentication/Email/EmailSender.java
+++
@@ -1,5 +0,0 @@
-package io.github.reconsolidated.weaskedapi.authentication.Email;
-
-public interface EmailSender {
-    void send(String to, String email);
-}
