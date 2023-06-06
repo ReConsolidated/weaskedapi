5c7250c8a780b185b5fd3d3c74e06ced2d3a4259
ReConsolidated <gracjanpasik@gmail.com>
2023-06-07 00:44:35 +0200

    docker build

--- src/main/java/io/github/reconsolidated/weaskedapi/WeAskedApiApplication.java
+++ src/main/java/io/github/reconsolidated/weaskedapi/WeAskedApiApplication.java
@@ -5,13 +5,8 @@ import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
